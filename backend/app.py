eb31399c2e8b08b9c46f1842cd0ea33e737db3a4
Mikask <darrenprasetya40@gmail.com>
2023-12-11 09:10:11 +0700

    docs: improve readibility of backend

--- backend/app.py
+++ backend/app.py
@@ -13,19 +13,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-tokenizer = IndoNLGTokenizer.from_pretrained('indobenchmark/indobart-v2')
-model = AutoModelForSeq2SeqLM.from_pretrained(
-    "Mikask/indobart-indonlg-nusax-500-jv-id")
+TOKENIZER_NAME = "indobenchmark/indobart-v2"
+MODEL_NAME = "Mikask/indobart-indonlg-nusax-500-jv-id"
+LANG_TOKEN = '[javanese]'
+DECODER_LANG_TOKEN = '[javanese]'
+
+tokenizer = IndoNLGTokenizer.from_pretrained(TOKENIZER_NAME)
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
 
 
 @app.get("/translate")
 async def translate(text: str):
+    """
+    This function takes a text string as input, translates it using a pretrained model,
+    and returns the translated text.
+    """
     try:
         encoded_text = text.encode('utf-8')
         decoded_text = encoded_text.decode('utf-8')
 
         inputs = tokenizer.prepare_input_for_generation([decoded_text], return_tensors='pt',
-                                                        lang_token='[javanese]', decoder_lang_token='[javanese]')
+                                                        lang_token=LANG_TOKEN, decoder_lang_token=DECODER_LANG_TOKEN)
 
         translated_tokens = model.generate(
             **inputs, max_length=inputs['input_ids'].shape[1] + 1)
@@ -34,5 +42,6 @@ async def translate(text: str):
             translated_tokens[0], skip_special_tokens=True).strip()
 
         return {"translation": result}
-    except Exception:
-        raise HTTPException(status_code=500, detail="Something went wrong")
+    except:
+        print(e)  # print the exception for debugging purposes
+        raise HTTPException(status_code=500, detail="Internal Server Error")
